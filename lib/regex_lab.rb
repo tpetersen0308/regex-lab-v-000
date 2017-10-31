aa66762822e27c367a24827c6e4749e360b1aa6d
Travis Petersen <travis.e.petersen@gmail.com>
2017-10-31 20:45:00 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -13,7 +13,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.scan(/^{[A-Z]}{\W}$/)
+  text.scan(/^[A-Z].*\W$/) ? true : false
 end
 
 def valid_phone_number?(phone)
